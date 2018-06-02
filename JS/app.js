d8ee648455c2091c8c2d26b3b90495786b94bce3
Tech-A33 <tech-a33@Tech-A33s-MacBook-Air.local>
2018-06-02 15:12:36 +0100

    Chicken moves, but not across divs on the board

---
+++ JS/app.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+  // Adding the chicken to the board
+  console.log('working');
+
+  $(function(){
+
+    var chicken = "<img class='chicken' src='images/chicken.png'>";
+    $(".player").append(chicken);
+
+    $(document).keydown(function(e){
+
+      var position = $(".player").position();
+
+
+      switch(e.keyCode)
+      {
+        case 37: //Left
+          $(".player").css('left',position.left - 20 + 'px');
+          break;
+        case 38: //Up
+          $(".player").css('top',position.top - 20 + 'px');
+          break;
+        case 39: //Right
+          $(".player").css('left',position.left + 20 + 'px');
+          break;
+        case 40: //Down
+          $(".player").css('top',position.top + 20 + 'px');
+          break;
+
+
+      }
+    })
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+});
