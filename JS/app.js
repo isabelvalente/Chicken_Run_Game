ee13a742a563ddc661cac9bc167de8bd3731a3c6
Tech-A33 <tech-a33@Tech-A33s-MacBook-Air.local>
2018-06-07 18:43:06 +0100

    Initial commit of fully functional game.

--- JS/app.js
+++ JS/app.js
@@ -3,32 +3,51 @@ $(document).ready(function(){
   var gameOver = false;
   var score = 0;
 
+  // Interval to move cars continuously
+  $(".startbtn").click(function(){
+    moveChicken(); //The chicken only moves once start button has been clicked
+    timer(); //The timer only starts once start button has been clicked
+    getCoords();
+    $(".instructions").hide();
+    $(".startbtn").hide();
+    // Cars start moving only when start button has been clicked
+    setInterval(moveFastCar1,1000);
+    setInterval(moveFastCar2,1000);
+    setInterval(moveSlowCar1,3000);
+    setInterval(moveSlowCar2,3000);
+    setInterval(moveSlowCar3,3000);
+    setInterval(moveFastestCar,3000);
+  });
+  setInterval(getCoords,50);
+
   // Function to move chicken
-  $(document).keydown(function(e){
+  function moveChicken(){
+    $(document).keydown(function(e){
 
-    var position = $(".player").position();
-    switch(e.keyCode) {
-      case 37: //Left
-      $(".player").css('left',position.left - 50 + 'px');
-      // console.log($(".player").position());
-      break;
-      case 38: //Up
-      $(".player").css('top',position.top - 50 + 'px');
-      break;
-      case 39: //Right
-      $(".player").css('left',position.left + 50 + 'px');
-      break;
-      case 40: //Down
-      $(".player").css('top',position.top + 50 + 'px');
-      break;
-    }
-  })
+      var position = $(".player").position();
+      switch(e.keyCode) {
+        case 37: //Left
+        $(".player").css('left',position.left - 50 + 'px');
+        break;
+        case 38: //Up
+        $(".player").css('top',position.top - 50 + 'px');
+        break;
+        case 39: //Right
+        $(".player").css('left',position.left + 50 + 'px');
+        break;
+        case 40: //Down
+        $(".player").css('top',position.top + 50 + 'px');
+        break;
+      }
+    })
 
-  // // Functions to move cars across the board once
+  }
+
+  // // Functions to move cars across the board
   function moveFastCar1(){
     $(".car6")
     .css("left", "1100px")
-    .animate({left: "-=1100px"}, 4000, 'linear', function(){
+    .animate({left: "-=1100px"}, 1500, 'linear', function(){
       moveFastCar1();
     })
   }
@@ -37,7 +56,7 @@ $(document).ready(function(){
   function moveFastCar2(){
     $(".car4")
     .css("left", "1100px")
-    .animate({left: "-=1100px"}, 4000, 'linear', function(){
+    .animate({left: "-=1100px"}, 5000, 'linear', function(){
       moveFastCar2();
     })
   }
@@ -51,11 +70,11 @@ $(document).ready(function(){
   }
 
   //
-  // //
+
   function moveSlowCar2(){
     $(".car2")
     .css("left", "0px")
-    .animate({left: "+=1100px"}, 4000, 'linear', function(){
+    .animate({left: "+=1100px"}, 3000, 'linear', function(){
       moveSlowCar2();
     })
   }
@@ -64,7 +83,7 @@ $(document).ready(function(){
   function moveSlowCar3(){
     $(".car5")
     .css("left", "0px")
-    .animate({left: "+=1100px"}, 4000, 'linear', function(){
+    .animate({left: "+=1100px"}, 2000, 'linear', function(){
       moveSlowCar3();
     })
   }
@@ -73,30 +92,16 @@ $(document).ready(function(){
   function moveFastestCar(){
     $(".car3")
     .css("left", "0px")
-    .animate({left: "+=1100px"}, 4000, 'linear', function(){
+    .animate({left: "+=1100px"}, 1700, 'linear', function(){
       moveFastestCar();
     })
   }
 
 
-  // Interval to move cars continuously
-  $(".startbtn").click(function(){
-    timer();
-    getCoords();
-    $(".instructions").hide();
-    $(".startbtn").hide();
-    setInterval(moveFastCar1,2000);
-    setInterval(moveFastCar2,2000);
-    setInterval(moveSlowCar1,3000);
-    setInterval(moveSlowCar2,3000);
-    setInterval(moveSlowCar3,3000);
-    setInterval(moveFastestCar,500);
-  });
-
-  setInterval(getCoords,50);
 
 
-  // Checking when Car object and Chicken Object intersect
+
+  // Accessing co-ordinates for cars and chicken in order to detect collisions
   var chicken = {};
   var carObj = {};
   var carObj2 = {};
@@ -131,7 +136,7 @@ $(document).ready(function(){
     carObj3.top = car3.offset().top;
     carObj3.right = carObj3.left + 80;
     carObj3.bottom = carObj3.top + 80;
-    // console.log($(".car3"));
+
 
     var car4 = $(".car4");
     carObj4.left = car4.offset().left;
@@ -152,6 +157,7 @@ $(document).ready(function(){
     carObj6.right = carObj6.left + 80;
     carObj6.bottom = carObj6.top + 80;
 
+
     // Calling collisions functions to cause collisions after each car object and the chicken intersect
     collision(chicken, carObj);
     collision(chicken, carObj2);
@@ -159,24 +165,36 @@ $(document).ready(function(){
     collision(chicken, carObj4);
     collision(chicken, carObj5);
     collision(chicken, carObj6);
+    // collision(chicken, board);
 
+    // If chicken is completely inside the finish line, display win message
     if (chicken.bottom < 129) {
       winner();
+    }
+
+    if (chicken.left < 120){
+      stopGame();
+    }
+
+    if (chicken.right > 1320){
+      stopGame();
+    }
+
+    if (chicken.bottom > 808){
+      stopGame();
     }
   }
 
   function collision(chicken, carObj) {
-
+    // if chicken collides with any car, display lose message
     if (chicken.left <= carObj.right && carObj.left <= chicken.right && chicken.top <= carObj.bottom && carObj.top <= chicken.bottom) {
     stopGame();
     }
-
   }
 
 
-  // Scoring
+  // Scoring - 1 point per second
   function timer (){
-    console.log("hey");
     counter = 0;
     score = setInterval(timer, 1000);
     function timer(){
@@ -185,7 +203,7 @@ $(document).ready(function(){
     }
   }
 
-
+  // Lose conditions
   function stopGame(){
     gameOver = true;
     $(".chicken").hide();
@@ -193,6 +211,7 @@ $(document).ready(function(){
     $(".lose").css("display", "block");
   }
 
+  // Win conditions
   function winner(){
     gameOver = true;
     $(".chicken").hide();
@@ -200,7 +219,9 @@ $(document).ready(function(){
     $(".win").css("display", "block");
   }
 
-
+  $('.restartbtn').click(function(){
+    location.reload();
+  });
 
 
 }); // end of $(document).ready()
